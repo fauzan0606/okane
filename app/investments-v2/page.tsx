914b8f876f7618c75df1272f23fb3b0340767032
fauzan0606 <90903292+fauzan0606@users.noreply.github.com>
2026-08-20 20:28:45 +0700

    Add investment v2 test page

---
+++ app/investments-v2/page.tsx
@@ -0,0 +1,11 @@
+import AppShell from "@/components/layout/AppShell";
+import Sidebar from "@/components/layout/Sidebar";
+import InvestmentDashboardV2 from "@/modules/investment/components/InvestmentDashboardV2";
+
+export default function InvestmentsV2Page() {
+  return (
+    <AppShell sidebar={<Sidebar />} header={null}>
+      <InvestmentDashboardV2 />
+    </AppShell>
+  );
+}
