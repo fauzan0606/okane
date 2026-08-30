2072137659964a28202f206815387eb0756e2b2e
fauzan0606 <90903292+fauzan0606@users.noreply.github.com>
2026-08-30 09:07:59 +0700

    use fixed investment dashboard v7

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,11 +1,11 @@
 import AppShell from "@/components/layout/AppShell";
 import Sidebar from "@/components/layout/Sidebar";
-import InvestmentDashboardV6 from "@/modules/investment/components/InvestmentDashboardV6";
+import InvestmentDashboardV7 from "@/modules/investment/components/InvestmentDashboardV7";
 
 export default function InvestmentsPage() {
   return (
     <AppShell sidebar={<Sidebar />} header={null}>
-      <InvestmentDashboardV6 />
+      <InvestmentDashboardV7 />
     </AppShell>
   );
 }
