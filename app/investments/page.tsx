f52cb39414b2f4217ceac0f5d33ae2c37753965a
fauzan0606 <90903292+fauzan0606@users.noreply.github.com>
2026-08-30 08:08:00 +0700

    refactor(investments): use account-centric dashboard

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,19 +1,11 @@
 import AppShell from "@/components/layout/AppShell";
 import Sidebar from "@/components/layout/Sidebar";
-import InvestmentDashboardFinal from "@/modules/investment/components/InvestmentDashboardFinal";
-import InvestmentAccountFormValidationFix from "@/modules/investment/components/InvestmentAccountFormValidationFix";
-import InvestmentTransactionSplitView from "@/modules/investment/components/InvestmentTransactionSplitView";
-import InvestmentTransactionLayoutFix from "@/modules/investment/components/InvestmentTransactionLayoutFix";
+import InvestmentDashboardV4 from "@/modules/investment/components/InvestmentDashboardV4";
 
 export default function InvestmentsPage() {
   return (
     <AppShell sidebar={<Sidebar />} header={null}>
-      <InvestmentAccountFormValidationFix>
-        <InvestmentTransactionLayoutFix />
-        <InvestmentTransactionSplitView>
-          <InvestmentDashboardFinal />
-        </InvestmentTransactionSplitView>
-      </InvestmentAccountFormValidationFix>
+      <InvestmentDashboardV4 />
     </AppShell>
   );
 }
