8438d75e083141770cc4ec6e57a63739ffa6ef71
fauzan0606 <90903292+fauzan0606@users.noreply.github.com>
2026-09-02 13:00:13 +0700

    fix(investments): mount duplicate balance cleanup

--- modules/investment/components/InvestmentDashboardV8.tsx
+++ modules/investment/components/InvestmentDashboardV8.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import InvestmentDashboardV7 from "./InvestmentDashboardV7";
+import InvestmentRdnBalanceCleanup from "./InvestmentRdnBalanceCleanup";
 
 export default function InvestmentDashboardV8() {
-  return <InvestmentDashboardV7 />;
+  return (
+    <>
+      <InvestmentDashboardV7 />
+      <InvestmentRdnBalanceCleanup />
+    </>
+  );
 }
